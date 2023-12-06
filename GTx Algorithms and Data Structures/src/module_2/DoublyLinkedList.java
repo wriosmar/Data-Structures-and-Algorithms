f3b7d19aed1e8f5d81234ac3df7f3edc6b4b7ddb
wriosmar <wriosmar@guillermos-mbp.lan>
2023-12-05 23:38:06 -0800

    Added constructor to DoublyLinkedList.

--- GTx Algorithms and Data Structures/src/module_2/DoublyLinkedList.java
+++ GTx Algorithms and Data Structures/src/module_2/DoublyLinkedList.java
@@ -1,5 +1,13 @@
 package module_2;
 
-public class DoublyLinkedList {
+public class DoublyLinkedList<T> {
+	private Node<T> head;
+	private Node<T> tail;
+	
+	// constructor; sets head and tail to null.
+	public DoublyLinkedList() {
+		head = null;
+		tail = null;
+	}
 
 }
