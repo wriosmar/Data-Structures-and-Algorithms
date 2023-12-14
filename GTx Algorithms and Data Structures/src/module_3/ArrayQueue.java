3298650af9b6f6380b275df143beb86aef8a0593
wriosmar <wriosmar@192.168.0.33>
2023-12-13 23:34:06 -0800

    Formatted comments in ArrayQueue

--- GTx Algorithms and Data Structures/src/module_3/ArrayQueue.java
+++ GTx Algorithms and Data Structures/src/module_3/ArrayQueue.java
@@ -23,19 +23,19 @@ public class ArrayQueue<T> {
 		return null;
 	}
 	
-	// returns the backing array of the queue
+	// returns the backing array of the queue.
 	public T[] getBackingArray() {
 		return backingArray;
 	}
 	
-	// returns the size of the queue
+	// returns the size of the queue.
 	public int size() {
 		return size;
 	}
 	
 	// copies the elements from original array to a new array double the size.
 	public Object[] resize() {
-		// create a new array double the size of the original
+		// create a new array double the size of the original.
 		Object[] newArray = new Object[backingArray.length * 2];
 		// copy the elements of the original to the new array.
 		for(int i = 0; i < backingArray.length; i++) {
