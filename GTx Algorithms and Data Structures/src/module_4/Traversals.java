bcd29e5d0e517de129b3349521c7c94e937cebce
wriosmar <wriosmar@gmail.com>
2023-12-22 12:38:44 -0800

    Completed Traversals class. Includes Pree, Post, and In Traversals.

--- GTx Algorithms and Data Structures/src/module_4/Traversals.java
+++ GTx Algorithms and Data Structures/src/module_4/Traversals.java
@@ -1,21 +1,50 @@
 package module_4;
 
 import java.util.List;
-// import java.util.ArrayList;
-// import java.util.LinkedList;
+import java.util.LinkedList;
 
 public class Traversals<T extends Comparable<? super T>> {
 	
 	public List<T> preorder(TreeNode<T> root) {
-		return null;
+		LinkedList<T> traverse = new LinkedList<T>();
+		return preHelp(root, traverse);
+	}
+	
+	private List<T> preHelp(TreeNode<T> node, List<T> list) {
+		if(node != null) {
+			list.add(node.getData());
+			preHelp(node.getLeft(), list);
+			preHelp(node.getRight(), list);
+		}
+		return list;
 	}
 	
 	public List<T> inorder(TreeNode<T> root) {
-		return null;
+		LinkedList<T> traverse = new LinkedList<T>();
+		return inHelp(root, traverse);
+	}
+	
+	private List<T> inHelp(TreeNode<T> node, List<T> list) {
+		if(node != null) {
+			inHelp(node.getLeft(), list);
+			list.add(node.getData());
+			inHelp(node.getRight(), list);
+		}
+		return list;
 	}
 	
 	public List<T> postorder(TreeNode<T> root) {
-		return null;
+		LinkedList<T> traverse = new LinkedList<T>();
+		return postHelp(root, traverse);
+	}
+	
+	private List<T> postHelp(TreeNode<T> node, List<T> list) {
+		if(node != null) {
+			postHelp(node.getLeft(), list);
+			postHelp(node.getRight(), list);
+			list.add(node.getData());
+		}
+		return list;
 	}
 
 }
---
+++ GTx Algorithms and Data Structures/src/module_4/Traversals_Tester.java
@@ -0,0 +1,10 @@
+package module_4;
+
+public class Traversals_Tester {
+
+	public static void main(String[] args) {
+		// create a test BST
+		
+	}
+
+}
