cd173215fb0091e7fbc5bd721a8583b3aa20d28b
wriosmar <wriosmar@gmail.com>
2023-12-21 18:05:13 -0800

    Added Traversals and TreeNode classes.

---
+++ GTx Algorithms and Data Structures/src/module_4/Traversals.java
@@ -0,0 +1,21 @@
+package module_4;
+
+import java.util.List;
+// import java.util.ArrayList;
+// import java.util.LinkedList;
+
+public class Traversals<T extends Comparable<? super T>> {
+	
+	public List<T> preorder(TreeNode<T> root) {
+		return null;
+	}
+	
+	public List<T> inorder(TreeNode<T> root) {
+		return null;
+	}
+	
+	public List<T> postorder(TreeNode<T> root) {
+		return null;
+	}
+
+}
