4951a77ace23c0fdd33aa28990f90427ca491589
wriosmar <wriosmar@gmail.com>
2024-01-02 18:29:35 -0800

    Added BSTNode & BST methods to be implemented.

--- GTx Algorithms and Data Structures/src/module_5/BST.java
+++ GTx Algorithms and Data Structures/src/module_5/BST.java
@@ -1,5 +1,24 @@
 package module_5;
 
-public class BST {
+public class BST<T extends Comparable<? super T>> {
+	private BSTNode<T> root;
+	private int size;
+	
+	public void add(T data) {
+		//TODO
+	}
+	
+	public T remove(T data) {
+		//TODO
+		return null;
+	}
+	
+	public BSTNode<T> getRoot() {
+		return root;
+	}
+	
+	public int size() {
+		return size;
+	}
 
 }
--- GTx Algorithms and Data Structures/src/module_5/BSTNode.java
+++ GTx Algorithms and Data Structures/src/module_5/BSTNode.java
@@ -1,5 +1,36 @@
 package module_5;
 
-public class BSTNode {
+public class BSTNode<T extends Comparable<? super T>> {
+	private T data;
+	private BSTNode<T> left;
+	private BSTNode<T> right;
+	
+	BSTNode(T data) {
+		this.data = data;
+	}
+	
+	T getData() {
+		return data;
+	}
+	
+	BSTNode<T> getLeft() {
+		return left;
+	}
+	
+	BSTNode<T> getRight() {
+		return right;
+	}
+	
+	void setData(T data) {
+		this.data = data;
+	}
+	
+	void setLeft(BSTNode<T> left) {
+		this.left = left;
+	}
+	
+	void setRight(BSTNode<T> right) {
+		this.right = right;
+	}
 
 }
