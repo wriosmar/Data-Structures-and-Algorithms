a14999c43fda538ba8f77e2a8fec38e3a55945ed
wriosmar <wriosmar@gmail.com>
2024-01-17 18:10:11 -0800

    Completed and tested ChainHashMap put method.

---
+++ GTx Algorithms and Data Structures/src/module_7/ChainHashMap_Tester.java
@@ -0,0 +1,13 @@
+package module_7;
+
+public class ChainHashMap_Tester {
+
+	public static void main(String[] args) {
+		ExternalChainingHashMap<Integer, Integer> test = new ExternalChainingHashMap<Integer, Integer>();
+		
+		test.put(6, 6);
+		test.put(32, 32);
+		
+	}
+
+}
--- GTx Algorithms and Data Structures/src/module_7/ExternalChainingHashMap.java
+++ GTx Algorithms and Data Structures/src/module_7/ExternalChainingHashMap.java
@@ -1,5 +1,8 @@
 package module_7;
 
+import java.util.NoSuchElementException;
+
+@SuppressWarnings("unused")
 public class ExternalChainingHashMap<K, V> {
 	
 	public static final int INITIAL_CAPACITY = 13;
@@ -8,12 +11,50 @@ public class ExternalChainingHashMap<K, V> {
 	private ExternalChainingMapEntry<K, V>[] table;
 	private int size;
 	
+	@SuppressWarnings("unchecked")
 	public ExternalChainingHashMap() {
 		table = (ExternalChainingMapEntry<K, V>[]) new ExternalChainingMapEntry[INITIAL_CAPACITY];
 	}
 	
 	public V put(K key, V value) {
-		//TODO
+		// check key & value are non null
+		if(key == null || value == null) {
+			throw new IllegalArgumentException("Key and Value can't be null!");
+		}
+		// check load factor does not exceed max load factor.
+		// if it does, resize (2 * oldLength) + 1.
+		if((size + 1) % table.length > MAX_LOAD_FACTOR) {
+			resizeBackingTable((2 * table.length) + 1);
+		}
+		
+		int hashcode = key.hashCode() % table.length;
+		
+		if(table[hashcode] == null) {
+			// create new LinkedList (ECME) at the index.
+			table[hashcode] = new ExternalChainingMapEntry<K, V>(key, value);
+		} else {
+			// check for duplicate keys at hash code index.
+			 ExternalChainingMapEntry<K, V> curr = table[hashcode];
+			
+			while(curr != null) {
+				K currKey = curr.getKey();
+				if(currKey.equals(key)) {
+					// update key's value and save old value.
+					V oldValue = curr.getValue();
+					curr.setValue(value);
+					return oldValue;
+				}
+				curr = curr.getNext();
+			}
+			
+			// add new <key, value> pair at head of LinkedList (ECME) at hash code index.
+			ExternalChainingMapEntry<K, V> head = new ExternalChainingMapEntry<K, V>(key, value);
+			head.setNext(table[hashcode]);
+			table[hashcode] = head;
+		}
+		
+		size++;
+				
 		return null;
 	}
 	
