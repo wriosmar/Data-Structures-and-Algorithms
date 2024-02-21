ca6d829f98f4c0197d101f8942d4c141395b6045
wriosmar <wriosmar@gmail.com>
2024-02-21 13:30:53 -0800

    Completed AVL rotations and update Height and Balancing Factor.

--- GTx Algorithms and Data Structures/src/module_8/AVL.java
+++ GTx Algorithms and Data Structures/src/module_8/AVL.java
@@ -3,36 +3,66 @@ package module_8;
 public class AVL<T extends Comparable<? super T>> {
 	
 	public void updateHeightAndBF(AVLNode<T> currentNode) {
-		//TODO
+		int leftChildHeight;
+		int rightChildHeight;
 		
+		if(currentNode.getLeft() == null) {
+			leftChildHeight = -1;
+		} else {
+			leftChildHeight = currentNode.getLeft().getHeight();
+		}
+		
+		if(currentNode.getRight() == null) {
+			rightChildHeight = -1;
+		} else {
+			rightChildHeight = currentNode.getRight().getHeight();
+		}
+		
+		currentNode.setHeight(Math.max(leftChildHeight, rightChildHeight) + 1);
+		currentNode.setBalanceFactor(leftChildHeight - rightChildHeight);
 	}
 	
 	public AVLNode<T> rotateLeft(AVLNode<T> currentNode){
-		//TODO
-		return null;
+		AVLNode<T> rotateNode = currentNode.getRight();
+		
+		currentNode.setRight(rotateNode.getLeft());
+		
+		rotateNode.setLeft(currentNode);
+		
+		updateHeightAndBF(currentNode);
+		updateHeightAndBF(rotateNode);
+		
+		return rotateNode;
 	}
 	
 	public AVLNode<T> rotateRight(AVLNode<T> currentNode){
-		//TODO
-		return null;
+		AVLNode<T> rotateNode = currentNode.getLeft();
+		
+		currentNode.setLeft(rotateNode.getRight());
+		
+		rotateNode.setRight(currentNode);
+		
+		updateHeightAndBF(currentNode);
+		updateHeightAndBF(rotateNode);
+		
+		return rotateNode;
 	}
 	
 	public AVLNode<T> balance(AVLNode<T> currentNode){
-		//TODO check conditions.
 		// update height and BF of current node.
 		updateHeightAndBF(currentNode);
 		
 		// condition for a right heavy tree.
 		if(currentNode.getBalanceFactor() < -1) {
 			// condition for a right-left rotation.
-			if(currentNode.getRight().getBalanceFactor() > 1) {
+			if(currentNode.getRight().getBalanceFactor() >= 1) {
 				currentNode.setRight(rotateRight(currentNode.getRight()));
 			}
 			currentNode = rotateLeft(currentNode);
 		} // condition for a left heavy tree.
 		else if(currentNode.getBalanceFactor() > 1) {
 			// condition for a left-right rotation.
-			if(currentNode.getLeft().getBalanceFactor() < -1) {
+			if(currentNode.getLeft().getBalanceFactor() <= -1) {
 				currentNode.setLeft(rotateLeft(currentNode.getLeft()));
 			}
 			currentNode = rotateRight(currentNode);
