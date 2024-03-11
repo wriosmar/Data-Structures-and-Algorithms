3f4ecb2774e45266661af34456c6ffc8e817bebc
wriosmar <wriosmar@gmail.com>
2024-03-11 13:35:35 -0700

    Added Sorting class; bubble, selection, insertion sort

---
+++ GTx Algorithms and Data Structures/src/module_9/Sorting.java
@@ -0,0 +1,22 @@
+package module_9;
+
+import java.util.Comparator;
+
+public class Sorting {
+	
+	// Implement Bubble Sort
+	public static <T> void bubbleSort(T[] arr, Comparator<T> comparator) {
+		//TODO
+	}
+	
+	// Implement Selection Sort
+	public static<T> void selectionSort(T[] arr, Comparator<T> comparator) {
+		//TODO
+	}
+	
+	// Implement Insertion Sort
+	public static<T> void insertionSort(T[] arr, Comparator<T> comparator) {
+		//TODO
+	}
+
+}
