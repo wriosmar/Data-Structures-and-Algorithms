f98fe02f9b6c2d30d634a5dd7184d1a625fb1024
wriosmar <wriosmar@gmail.com>
2024-03-14 16:03:31 -0700

    Created a swap method. Implemented Bubble Sort.

--- GTx Algorithms and Data Structures/src/module_9/Sorting.java
+++ GTx Algorithms and Data Structures/src/module_9/Sorting.java
@@ -6,7 +6,21 @@ public class Sorting {
 	
 	// Implement Bubble Sort
 	public static <T> void bubbleSort(T[] arr, Comparator<T> comparator) {
-		//TODO
+		int stop_index = arr.length - 1;
+		
+		while(stop_index != 0) {
+			int i = 0;
+			int last_swapped = 0;
+			
+			while(i < stop_index) {
+				if(comparator.compare(arr[i], arr[i + 1]) > 1) {
+					arr = swap(arr, i, i + 1);
+					last_swapped = i;
+				}
+				i++;
+			}
+			stop_index = last_swapped;
+		}
 	}
 	
 	// Implement Selection Sort
@@ -17,6 +31,15 @@ public class Sorting {
 	// Implement Insertion Sort
 	public static<T> void insertionSort(T[] arr, Comparator<T> comparator) {
 		//TODO
+	}
+	
+	private static <T> T[] swap(T[] arr, int index_1, int index_2) {
+		T hold = arr[index_1];
+		
+		arr[index_1] = arr[index_2];
+		arr[index_2] = hold;
+		
+		return arr;
 	}
 
 }
