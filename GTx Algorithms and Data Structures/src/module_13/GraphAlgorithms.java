4b3c90395d4e40787e897b636c6564ec92e0428f
wriosmar <wriosmar@gmail.com>
2024-04-25 15:19:48 -0700

    Implemented BFS.

--- GTx Algorithms and Data Structures/src/module_13/GraphAlgorithms.java
+++ GTx Algorithms and Data Structures/src/module_13/GraphAlgorithms.java
@@ -1,16 +1,46 @@
 package module_13;
 
 import java.util.List;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Set;
 
 public class GraphAlgorithms {
 	
 	public static <T> List<Vertex<T>> bfs(Vertex<T> start, Graph<T> graph) {
-		//TODO
-		return null;
+		Set<Vertex<T>> VS = new HashSet<>();
+		Queue<Vertex<T>> Q = new LinkedList<>();
+		
+		List<Vertex<T>> visited = new LinkedList<>();
+		
+		VS.add(start);
+		Q.add(start);
+		
+		visited.add(start);
+		
+		while(!Q.isEmpty()) {
+			Vertex<T> currV = Q.remove();
+			List<VertexDistance<T>> neighbors = graph.getAdjList().getOrDefault(currV, new ArrayList<>());
+			
+			for(VertexDistance<T> currN : neighbors) {
+				if(VS.contains(currN.getVertex())) {
+					VS.add(currN.getVertex());
+					Q.add(currN.getVertex());
+					
+					visited.add(currN.getVertex());
+				}
+			}
+		}
+		
+		return visited;
 	}
 	
 	public static <T> List<Vertex<T>> dfs(Vertex<T> start, Graph<T> graph) {
-		//TODO
+		Set<Vertex<T>> VS = new HashSet<>();
 		return null;
 	}
 
